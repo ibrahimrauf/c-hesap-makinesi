7509a56d7987fd65e32107119e9625ebe0e4627a
ibrahimrauf <119485615+ibrahimrauf@users.noreply.github.com>
2022-12-30 22:58:03 +0300

    Add files via upload

---
+++ TersteAdyazdirma-main/Tersden ad yazdırma/Program.cs
@@ -0,0 +1,9 @@
+﻿string cumle, tersCumle = "";
+Console.Write("cümle giriniz:");
+cumle = Console.ReadLine();
+for(int i=cumle.Length-1;i>=0;i--)
+{
+tersCumle += cumle[i];
+}
+Console.WriteLine(tersCumle);
+Console.ReadKey();
